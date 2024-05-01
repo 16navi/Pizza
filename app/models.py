d334b352dc845df3a3d8f1bf6f5316e64d85d5a0
Lei Ivan Juarez <24514@burnside.school.nz>
2024-05-02 11:19:18 +1200

    added /app to follow the format in the email

---
+++ app/models.py
@@ -0,0 +1,7 @@
+from pizza import DB
+
+class Pizza(db.Model):
+    __tablename__ = "Pizza"
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.Text())
+    description = db.Column(db.Text()) 
