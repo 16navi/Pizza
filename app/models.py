21c2070c44141cb144482bd3a7f4dc11f8237185
Lei Ivan Juarez <24514@burnside.school.nz>
2024-05-06 11:00:25 +1200

    nothing new really

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from app.routes import db
 
+
 class Pizza(db.Model):
     __tablename__ = "Pizza"
     id = db.Column(db.Integer, primary_key = True)
@@ -11,3 +12,24 @@ class Pizza(db.Model):
 
     def __repr__(self):
         return self.name
+
+
+class Base(db.Model):
+    __tablename__ = "Base"
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.Text())
+    description = db.Column(db.Text())
+    gf = db.Column(db.Integer())
+    vegan = db.Column(db.Integer())
+    vege = db.Column(db.Integer())
+
+
+    def __repr__(self):
+        return self.name
+
+
+class Topping(db.Model):
+    __tablename__ = "Topping"
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.Text())
+    description = db.Column(db.Text())
