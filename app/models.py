f78fac2818a471454288158f5e86d16d1b660062
Lei Ivan Juarez <24514@burnside.school.nz>
2024-05-09 11:00:40 +1200

    added many-to-many relationship.

--- app/models.py
+++ app/models.py
@@ -1,14 +1,23 @@
 from app.routes import db
 
 
+PizzaTopping = db.Table('PizzaTopping',
+    db.Column('pid', db.Integer, db.ForeignKey('Pizza.id')),
+    db.Column('tid', db.Integer, db.ForeignKey('Topping.id'))
+    )
+
+
 class Pizza(db.Model):
-    __tablename__ = "Pizza"
+    __tablename__ = 'Pizza'
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.Text())
     description = db.Column(db.Text())
     photo = db.Column(db.Text())
-    base_id = db.Column(db.Integer, db.ForeignKey("Base.id"))
-    base = db.relationship("Base", backref = "pizzas")
+    base_id = db.Column(db.Integer, db.ForeignKey('Base.id'))
+    base = db.relationship('Base', backref = 'pizzas_base')
+    toppings = db.relationship('Topping',
+        secondary = 'PizzaTopping', 
+        back_populates = 'pizzas')
 
 
     def __repr__(self):
@@ -16,7 +25,7 @@ class Pizza(db.Model):
 
 
 class Base(db.Model):
-    __tablename__ = "Base"
+    __tablename__ = 'Base'
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.Text())
     description = db.Column(db.Text())
@@ -30,10 +39,13 @@ class Base(db.Model):
 
 
 class Topping(db.Model):
-    __tablename__ = "Topping"
+    __tablename__ = 'Topping'
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.Text())
     description = db.Column(db.Text())
+    pizzas = db.relationship('Pizza',
+        secondary = 'PizzaTopping',
+        back_populates = 'toppings')
 
 
     def __repr__(self):
